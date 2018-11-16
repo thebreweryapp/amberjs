4e445a32f64ad6def612878fa500ec05c3093cc5
Eduard Likwong <eduard_likwong@yahoo.com>
2018-11-16 12:28:55 +0800

    Added bash script for initializing db.js

---
+++ scripts/init_db.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cat <<EOM >database.js
+module.exports = {
+    development: {
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE_NAME,
+        uri: process.env.DATABASE_URI
+    },
+    test: {
+        username: 'root',
+        password: null,
+        database: 'boilerplate_test',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+        logging: null
+    },
+    production: process.env.DATABASE_URL
+};
+EOM
