0d7b41ee4dd3579278bb543fa5ea5497ecd36ae5
Eduard Likwong <eduard_likwong@yahoo.com>
2018-11-16 16:47:58 +0800

    Removed test config db, restored gitignore

--- config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    development: {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE_NAME,
-        uri: process.env.DATABASE_URI
-    }
-};
