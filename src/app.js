379686e188d487c3fbdf76afa986578ae8b3b8e4
Joshua Mante <joshuamante@SP-JMante-MacBookPro2017.local>
2019-09-26 12:24:49 +0800

    updated error handling on brew

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ require('module').Module._initPaths();
 const { brew } = require('@amberjs/core');
 const config = require('config');
 
-brew(config, brewed => {
+brew(config, (err, brewed) => {
+  if (err) throw err;
   const app = brewed.getServer();
   app.start().catch(error => {
     app.logger.error(error.stack);
